a2fcf60478b645b1c0006d3d31b82a32bf42624f
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-05-28 19:36:05 -0600

    implement approval card with reusable component, closes #7

--- src/components/CommentDetail.js
+++ src/components/CommentDetail.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import faker from 'faker';
 
 const CommentDetail=(props)=>{
-    
-    console.log(props);
+
     return (
         <div>
             <div className="comment">
         <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()}/>
+        <img alt="avatar" src={props.img}/>
         </a>
         <div className="content">
             <a href="/" className="author">
@@ -20,7 +18,7 @@ const CommentDetail=(props)=>{
                     </span>
             </div>
                 <div className="text">
-                Nice Blog Post
+                {props.comment}
                 </div>
         </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './components/CommentDetail';
+import faker from 'faker'
+import ApprovalCard from './components/ApprovalCard';
 
 const App=()=>{
     return (
     <div className="ui container comments">
-    <CommentDetail author="Osas" time="Today at 6:00PM"/>
-    <CommentDetail author="Jane" time="Today at 2:00PM"/>
-    <CommentDetail author="Sam" time="Today at 10:00AM"/>
+    <ApprovalCard>
+    <CommentDetail 
+    author="Osas" 
+    time="Today at 6:00PM" 
+    comment = "So inspirational" 
+    img={faker.image.avatar()}
+    />
+    </ApprovalCard>
+    
+    <ApprovalCard>
+    <CommentDetail author="Jane" 
+    time="Yesterday at 2:00PM" 
+    comment = "Lovely" 
+    img={faker.image.avatar()}/>
+    </ApprovalCard>
+
+    <ApprovalCard>
+    <CommentDetail 
+    author="Sam" 
+    time="Today at 10:00AM" 
+    comment = "Nice Blog Post" 
+    img={faker.image.avatar()}/>
+    </ApprovalCard>
+
     </div>)
 }
 
